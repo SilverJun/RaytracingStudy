fa22713ea5af9f50961d1dc54f676f89956e53b0
SilverJun <jangej1031@naver.com>
2020-12-15 00:06:38 +0900

    First step

---
+++ Ray Tracing in One Weekend/Ray Tracing in One Weekend.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int IMAGE_WIDTH = 256;
+	const int IMAGE_HEIGHT = 256;
+
+	std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+
+	for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
+	{
+		for (int i = 0; i < IMAGE_WIDTH; ++i)
+		{
+			auto r = double(i) / (IMAGE_WIDTH - 1);
+			auto g = double(j) / (IMAGE_HEIGHT - 1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+	
+	return 0;
+}
