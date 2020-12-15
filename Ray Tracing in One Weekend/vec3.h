cf7e5d8dab23c9b1d045c71a3dd8836aff307988
SilverJun <jangej1031@naver.com>
2020-12-16 01:33:40 +0900

    8.2 Limiting the Number of Child Rays

--- Ray Tracing in One Weekend/vec3.h
+++ Ray Tracing in One Weekend/vec3.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "rtweekend.h"
 #include <cmath>
 #include <iostream>
 
@@ -37,6 +38,14 @@ public:
 
     double length_squared() const {
         return x * x + y * y + z * z;
+    }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 	
 public:
@@ -103,4 +112,12 @@ inline vec3 cross(const vec3& u, const vec3& v)
 inline vec3 unit_vector(vec3 v)
 {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
 }
