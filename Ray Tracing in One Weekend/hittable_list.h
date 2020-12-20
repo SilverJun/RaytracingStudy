515f86eb710cc7d010bf864fbf9f3a59be46f270
SilverJun <jangej1031@naver.com>
2020-12-20 22:44:38 +0900

    11. Positionable Camera

--- Ray Tracing in One Weekend/hittable_list.h
+++ Ray Tracing in One Weekend/hittable_list.h
@@ -9,37 +9,34 @@ using std::shared_ptr;
 using std::make_shared;
 using std::vector;
 
-class hittable_list :public hittable
-{
+class hittable_list : public hittable {
 public:
-	hittable_list() = default;
-	hittable_list(shared_ptr<hittable> object) { add(object); }
+    hittable_list() = default;
+    hittable_list(shared_ptr<hittable> object) { add(object); }
 
-	void clear()
-	{
-		objects.clear();
-	}
+    void clear() { objects.clear(); }
+    void add(shared_ptr<hittable> object) { objects.push_back(object); }
 
-	void add(shared_ptr<hittable> object) { objects.push_back(object); }
-
-	bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override
-	{
-		hit_record temp_rec;
-		bool hit_anything = false;
-		auto closest_so_far = t_max;
-
-		for (const auto& object : objects) {
-			if (object->hit(r, t_min, closest_so_far, temp_rec)) {
-				hit_anything = true;
-				closest_so_far = temp_rec.t;
-				rec = temp_rec;
-			}
-		}
-
-		return hit_anything;
-	}
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
 public:
-	vector<shared_ptr<hittable>> objects;
+    std::vector<shared_ptr<hittable>> objects;
 };
 
+
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    auto hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (const auto& object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
