b40e3b3ed02f486781052ff6bdeb06829f339760
SilverJun <jangej1031@naver.com>
2020-12-24 01:50:46 +0900

    Fix multithreading artifacts

--- Ray Tracing in One Weekend/material.h
+++ Ray Tracing in One Weekend/material.h
@@ -13,39 +13,27 @@ public:
 
 class lambertian : public material {
 public:
-    lambertian(const color& a) : albedo(a) {}
-
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
-	{
-        auto scatter_direction = rec.normal + random_unit_vector();
-
-        // Catch degenerate scatter direction
-        if (scatter_direction.near_zero())
-            scatter_direction = rec.normal;
-    	
-        scattered = ray(rec.p, scatter_direction);
+    lambertian(const vec3& a) : albedo(a) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        scattered = ray(rec.p, target - rec.p);
         attenuation = albedo;
         return true;
     }
 
-public:
-    color albedo;
+    vec3 albedo;
 };
 
-class metal : public material
-{
-public:
-	metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
-	{
+class metal : public material {
+public:
+    metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.dir), rec.normal);
         scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
-        return dot(scattered.dir, rec.normal) > 0;
-	}
-	
-public:
-    color albedo;
-    double fuzz;
+        return (dot(scattered.dir, rec.normal) > 0);
+    }
+    vec3 albedo;
+    float fuzz;
 };
--- Ray Tracing in One Weekend/rtweekend.h
+++ Ray Tracing in One Weekend/rtweekend.h
@@ -17,14 +17,14 @@ inline double degrees_to_radians(double degrees)
 }
 
 inline double random_double(double min, double max) {
-    static std::uniform_real_distribution<double> distribution(min, max);
-    static std::mt19937 generator;
+    static thread_local std::uniform_real_distribution<double> distribution(min, max);
+    static thread_local std::mt19937 generator;
     return distribution(generator);
 }
 
 inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
+    static thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static thread_local std::mt19937 generator;
     return distribution(generator);
 }
 
